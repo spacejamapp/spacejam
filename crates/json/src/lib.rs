92a70c60d3106d4895fd12b646efb0d535544560
clearloop <tianyi.gc@gmail.com>
2024-12-01 04:15:37 +0700

    feat(json): introduce package json

--- crates/json/src/lib.rs
+++ crates/json/src/lib.rs
@@ -2,26 +2,76 @@
 //!
 //! Now using hex as the default encoding.
 use anyhow::Result;
-use serde::{de::DeserializeOwned, Serialize};
 
 /// A trait for types that can be encoded and decoded to and from JSON.
-pub trait Json: Sized {
-    type Target: Serialize + DeserializeOwned + From<Self> + TryInto<Self, Error = anyhow::Error>;
-
+pub trait Json<Target>: Sized {
     /// Converts the value to its JSON representation.
-    fn json(self) -> Self::Target {
-        Self::Target::from(self)
+    fn to_json(self) -> Target;
+
+    /// Converts the value from its JSON representation.
+    fn from_json(json: Target) -> Result<Self>;
+}
+
+impl<M, N> Json<Option<M>> for Option<N>
+where
+    N: Json<M>,
+{
+    fn to_json(self) -> Option<M> {
+        self.map(|v| v.to_json())
     }
 
-    /// Encodes the value to a JSON string.
-    fn to_json(self) -> Result<String> {
-        let target: Self::Target = self.into();
-        serde_json::to_string_pretty(&target).map_err(Into::into)
-    }
-
-    /// Decodes the value from a JSON string.
-    fn from_json(json: &str) -> Result<Self> {
-        let target: Self::Target = serde_json::from_str(json)?;
-        Self::Target::try_into(target).map_err(Into::into)
+    fn from_json(json: Option<M>) -> Result<Self> {
+        json.map(|v| N::from_json(v)).transpose()
     }
 }
+
+impl<M, N> Json<Vec<M>> for Vec<N>
+where
+    N: Json<M>,
+{
+    fn to_json(self) -> Vec<M> {
+        self.into_iter().map(|v| v.to_json()).collect()
+    }
+
+    fn from_json(json: Vec<M>) -> Result<Self> {
+        json.into_iter().map(|v| N::from_json(v)).collect()
+    }
+}
+
+impl Json<String> for Vec<u8> {
+    fn to_json(self) -> String {
+        format!("0x{}", hex::encode(self))
+    }
+
+    fn from_json(json: String) -> Result<Self> {
+        let bytes = hex::decode(json.trim_start_matches("0x"))?;
+        Ok(bytes)
+    }
+}
+
+macro_rules! impl_json {
+    ($($len:expr),*) => {
+        $(
+            impl Json<String> for [u8; $len] {
+                fn to_json(self) -> String {
+                    format!("0x{}", hex::encode(self))
+                }
+
+                fn from_json(json: String) -> Result<Self> {
+                    let bytes = hex::decode(json.trim_start_matches("0x"))?;
+                    let len = bytes.len();
+
+                    Ok(bytes.try_into().map_err(|_| {
+                        anyhow::anyhow!(
+                            "Invalid hex string, target length is {len}, actual length is {actual}",
+                            len = $len,
+                            actual = len
+                        )
+                    })?)
+                }
+            }
+        )*
+    };
+}
+
+impl_json!(1, 2, 3, 4, 8, 16, 32, 64, 128, 144, 256, 784);
