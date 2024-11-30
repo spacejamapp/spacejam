01809d734cefb40279c8b9352447ff0500977241
clearloop <tianyi.gc@gmail.com>
2024-11-30 20:26:14 +0700

    feat(core-derive): introduce conversion for the json types

--- crates/core/derive/src/lib.rs
+++ crates/core/derive/src/lib.rs
@@ -1,5 +1,5 @@
 use proc_macro::TokenStream;
-use quote::ToTokens;
+use quote::quote;
 use syn::{parse_macro_input, Fields, Ident, ItemStruct};
 
 /// Derives a struct to implement the `Json` trait.
@@ -13,15 +13,16 @@ pub fn json_derive(input: TokenStream) -> TokenStream {
     let mut json = input.clone();
 
     // 1. Append Json to the struct name
-    let name = &json.ident;
-    json.ident = Ident::new(&format!("{name}Json"), name.span());
+    let name = json.ident.clone();
+    let json_name = Ident::new(&format!("{name}Json"), name.span());
+    json.ident = json_name.clone();
 
     // 2. Clean attrs from the original struct except for doc
     json.attrs.retain(|attr| attr.path().is_ident("doc"));
 
-    // 3. Modify [u8; N] fields to String
-    //
-    // TODO: support this encoding with attributes on fields.
+    // 3. Modify [u8; N] fields and fields with the #[json(hex)] attribute to String
+    let mut hex_fields = Vec::new();
+    let mut other_fields = Vec::new();
     if let Fields::Named(ref mut fields) = json.fields {
         for field in &mut fields.named {
             // Check for the #[json(hex)] attribute
@@ -33,21 +34,55 @@ pub fn json_derive(input: TokenStream) -> TokenStream {
                         .to_string()
                         == "hex".to_string()
             }) {
-                field.ty = syn::parse_quote! { String }; // Change to String
-                continue; // Skip further checks for this field
+                hex_fields.push(field.ident.clone());
+                field.ty = syn::parse_quote! { String };
+                continue;
             }
 
-            let syn::Type::Array(ref array_type) = field.ty else {
-                continue; // Skip if not an array type
-            };
-            let syn::Type::Path(ref path_type) = *array_type.elem else {
-                continue; // Skip if not a path type
-            };
-            if path_type.path.is_ident("u8") {
-                field.ty = syn::parse_quote! { String };
+            if let syn::Type::Array(ref array_type) = field.ty {
+                if let syn::Type::Path(ref path_type) = *array_type.elem {
+                    if path_type.path.is_ident("u8") {
+                        hex_fields.push(field.ident.clone());
+                        field.ty = syn::parse_quote! { String };
+                        continue;
+                    }
+                }
             }
+
+            other_fields.push(field.ident.clone());
         }
     }
 
-    json.to_token_stream().into()
+    // 4. Append attributes to the json struct
+    json.attrs.extend(vec![syn::parse_quote! {
+        #[derive(serde::Serialize, serde::Deserialize)]
+    }]);
+
+    quote! {
+        #json
+
+        impl TryFrom<#json_name> for #name {
+            type Error = anyhow::Error;
+
+            fn try_from(value: #json_name) -> anyhow::Result<Self> {
+                Ok(#name {
+                    #(#other_fields: value.#other_fields,)*
+                    #(#hex_fields: hex::decode(value.#hex_fields.trim_start_matches("0x"))?
+                        .try_into()
+                        .map_err(|_| anyhow::anyhow!("Invalid hex string"))?,
+                    )*
+                })
+            }
+        }
+
+        impl From<#name> for #json_name {
+            fn from(value: #name) -> Self {
+                #json_name {
+                    #(#other_fields: value.#other_fields,)*
+                    #(#hex_fields: hex::encode(value.#hex_fields),)*
+                }
+            }
+        }
+    }
+    .into()
 }
