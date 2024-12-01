b3b640e5ea3f09859c5b59506006b39316ef7883
clearloop <tianyi.gc@gmail.com>
2024-12-01 22:01:42 +0700

    feat(codec): introduce basic serializer of jamcodec

--- crates/codec/src/lib.rs
+++ crates/codec/src/lib.rs
@@ -1,6 +1,9 @@
 //! JAMCodec based on the parity scale codec
 
+mod error;
 mod json;
+mod ser;
 
 pub use codec_derive::Json;
+pub use error::Error;
 pub use json::Json;
---
+++ crates/codec/src/ser.rs
@@ -0,0 +1,392 @@
+//! JAMCodec serialization implementation
+
+use crate::Error;
+use serde::ser;
+
+type Result<T> = std::result::Result<T, Error>;
+
+/// Serializer for JAMCodec
+pub struct Serializer {
+    output: Vec<u8>,
+}
+
+impl ser::Serializer for Serializer {
+    type Ok = ();
+    type Error = Error;
+
+    type SerializeSeq = Self;
+    type SerializeTuple = Self;
+    type SerializeTupleStruct = Self;
+    type SerializeTupleVariant = Self;
+    type SerializeMap = Self;
+    type SerializeStruct = Self;
+    type SerializeStructVariant = Self;
+
+    fn serialize_bool(mut self, v: bool) -> Result<()> {
+        if v {
+            self.output.push(1);
+        } else {
+            self.output.push(0);
+        }
+        Ok(())
+    }
+
+    fn serialize_i8(mut self, v: i8) -> Result<()> {
+        self.output.extend_from_slice(&[v as u8]);
+        Ok(())
+    }
+
+    fn serialize_u8(mut self, v: u8) -> Result<()> {
+        self.output.push(v);
+        Ok(())
+    }
+
+    fn serialize_i16(mut self, v: i16) -> Result<()> {
+        self.output.extend_from_slice(&v.to_le_bytes());
+        Ok(())
+    }
+
+    fn serialize_u16(mut self, v: u16) -> Result<()> {
+        self.output.extend_from_slice(&v.to_le_bytes());
+        Ok(())
+    }
+
+    fn serialize_i32(mut self, v: i32) -> Result<()> {
+        self.output.extend_from_slice(&v.to_le_bytes());
+        Ok(())
+    }
+
+    fn serialize_u32(mut self, v: u32) -> Result<()> {
+        self.output.extend_from_slice(&v.to_le_bytes());
+        Ok(())
+    }
+
+    fn serialize_i64(mut self, v: i64) -> Result<()> {
+        self.output.extend_from_slice(&v.to_le_bytes());
+        Ok(())
+    }
+
+    fn serialize_u64(mut self, v: u64) -> Result<()> {
+        self.output.extend_from_slice(&v.to_le_bytes());
+        Ok(())
+    }
+
+    fn serialize_f32(mut self, v: f32) -> Result<()> {
+        self.output.extend_from_slice(&v.to_le_bytes());
+        Ok(())
+    }
+
+    fn serialize_f64(mut self, v: f64) -> Result<()> {
+        self.output.extend_from_slice(&v.to_le_bytes());
+        Ok(())
+    }
+
+    fn serialize_str(mut self, v: &str) -> Result<()> {
+        self.output.extend_from_slice(v.as_bytes());
+        Ok(())
+    }
+
+    fn serialize_bytes(mut self, v: &[u8]) -> Result<()> {
+        self.output.extend_from_slice(v);
+        Ok(())
+    }
+
+    fn serialize_char(mut self, v: char) -> Result<()> {
+        self.output.push(v as u8);
+        Ok(())
+    }
+
+    fn serialize_none(mut self) -> Result<()> {
+        self.output.push(0);
+        Ok(())
+    }
+
+    fn serialize_some<T>(mut self, value: &T) -> Result<()>
+    where
+        T: ser::Serialize + ?Sized,
+    {
+        self.output.push(1);
+        value.serialize(self)
+    }
+
+    fn serialize_unit(mut self) -> Result<()> {
+        self.output.push(0);
+        Ok(())
+    }
+
+    fn serialize_unit_variant(
+        self,
+        _name: &'static str,
+        _variant_index: u32,
+        _variant: &'static str,
+    ) -> Result<()> {
+        Err(anyhow::anyhow!("Unit variant not supported").into())
+    }
+
+    fn serialize_newtype_struct<T>(self, _name: &'static str, value: &T) -> Result<()>
+    where
+        T: ser::Serialize + ?Sized,
+    {
+        value.serialize(self)
+    }
+
+    fn collect_map<K, V, I>(self, _iter: I) -> Result<()>
+    where
+        K: ser::Serialize,
+        V: ser::Serialize,
+        I: IntoIterator<Item = (K, V)>,
+    {
+        Err(anyhow::anyhow!("Map not supported").into())
+    }
+
+    fn serialize_seq(self, _len: Option<usize>) -> Result<Self::SerializeSeq> {
+        Err(anyhow::anyhow!("Seq not supported").into())
+    }
+
+    fn serialize_tuple(self, _len: usize) -> Result<Self::SerializeTuple> {
+        Err(anyhow::anyhow!("Tuple not supported").into())
+    }
+
+    fn serialize_tuple_struct(
+        self,
+        _name: &'static str,
+        _len: usize,
+    ) -> Result<Self::SerializeTupleStruct> {
+        Err(anyhow::anyhow!("Tuple struct not supported").into())
+    }
+
+    fn serialize_tuple_variant(
+        self,
+        _name: &'static str,
+        _variant_index: u32,
+        _variant: &'static str,
+        _len: usize,
+    ) -> Result<Self::SerializeTupleVariant> {
+        Err(anyhow::anyhow!("Tuple variant not supported").into())
+    }
+
+    fn serialize_map(self, _len: Option<usize>) -> Result<Self::SerializeMap> {
+        Err(anyhow::anyhow!("Map not supported").into())
+    }
+
+    fn serialize_struct(self, _name: &'static str, _len: usize) -> Result<Self::SerializeStruct> {
+        Err(anyhow::anyhow!("Struct not supported").into())
+    }
+
+    fn serialize_struct_variant(
+        self,
+        _name: &'static str,
+        _variant_index: u32,
+        _variant: &'static str,
+        _len: usize,
+    ) -> Result<Self::SerializeStructVariant> {
+        Err(anyhow::anyhow!("Struct variant not supported").into())
+    }
+
+    fn serialize_unit_struct(self, _name: &'static str) -> Result<()> {
+        self.serialize_unit()
+    }
+
+    fn serialize_newtype_variant<T: ?Sized>(
+        self,
+        _name: &'static str,
+        _variant_index: u32,
+        _variant: &'static str,
+        _value: &T,
+    ) -> Result<()> {
+        Err(anyhow::anyhow!("Newtype variant not supported").into())
+    }
+
+    fn collect_seq<I>(self, _iter: I) -> Result<()>
+    where
+        I: IntoIterator,
+        <I as IntoIterator>::Item: ser::Serialize,
+    {
+        Err(anyhow::anyhow!("Seq not supported").into())
+    }
+
+    fn collect_str<T>(self, _value: &T) -> Result<()>
+    where
+        T: ?Sized + std::fmt::Display,
+    {
+        Err(anyhow::anyhow!("Str not supported").into())
+    }
+
+    fn serialize_i128(mut self, v: i128) -> Result<()> {
+        self.output.extend_from_slice(&v.to_le_bytes());
+        Ok(())
+    }
+
+    fn serialize_u128(mut self, v: u128) -> Result<()> {
+        self.output.extend_from_slice(&v.to_le_bytes());
+        Ok(())
+    }
+
+    fn is_human_readable(&self) -> bool {
+        false
+    }
+}
+
+// The following 7 impls deal with the serialization of compound types like
+// sequences and maps. Serialization of such types is begun by a Serializer
+// method and followed by zero or more calls to serialize individual elements of
+// the compound type and one call to end the compound type.
+//
+// This impl is SerializeSeq so these methods are called after `serialize_seq`
+// is called on the Serializer.
+impl ser::SerializeSeq for Serializer {
+    // Must match the `Ok` type of the serializer.
+    type Ok = ();
+    // Must match the `Error` type of the serializer.
+    type Error = Error;
+
+    // Serialize a single element of the sequence.
+    fn serialize_element<T>(&mut self, _value: &T) -> Result<()>
+    where
+        T: ?Sized + ser::Serialize,
+    {
+        Err(anyhow::anyhow!("Seq not supported").into())
+    }
+
+    // Close the sequence.
+    fn end(self) -> Result<()> {
+        Ok(())
+    }
+}
+
+// Same thing but for tuples.
+impl ser::SerializeTuple for Serializer {
+    type Ok = ();
+    type Error = Error;
+
+    fn serialize_element<T>(&mut self, _value: &T) -> Result<()>
+    where
+        T: ?Sized + ser::Serialize,
+    {
+        Err(anyhow::anyhow!("Tuple not supported").into())
+    }
+
+    fn end(self) -> Result<()> {
+        Ok(())
+    }
+}
+
+// Same thing but for tuple structs.
+impl ser::SerializeTupleStruct for Serializer {
+    type Ok = ();
+    type Error = Error;
+
+    fn serialize_field<T>(&mut self, _value: &T) -> Result<()>
+    where
+        T: ?Sized + ser::Serialize,
+    {
+        Err(anyhow::anyhow!("Tuple struct not supported").into())
+    }
+
+    fn end(self) -> Result<()> {
+        Ok(())
+    }
+}
+
+// Tuple variants are a little different. Refer back to the
+// `serialize_tuple_variant` method above:
+//
+//    self.output += "{";
+//    variant.serialize(&mut *self)?;
+//    self.output += ":[";
+//
+// So the `end` method in this impl is responsible for closing both the `]` and
+// the `}`.
+impl ser::SerializeTupleVariant for Serializer {
+    type Ok = ();
+    type Error = Error;
+
+    fn serialize_field<T>(&mut self, _value: &T) -> Result<()>
+    where
+        T: ?Sized + ser::Serialize,
+    {
+        Err(anyhow::anyhow!("Tuple variant not supported").into())
+    }
+
+    fn end(self) -> Result<()> {
+        Ok(())
+    }
+}
+
+// Some `Serialize` types are not able to hold a key and value in memory at the
+// same time so `SerializeMap` implementations are required to support
+// `serialize_key` and `serialize_value` individually.
+//
+// There is a third optional method on the `SerializeMap` trait. The
+// `serialize_entry` method allows serializers to optimize for the case where
+// key and value are both available simultaneously. In JSON it doesn't make a
+// difference so the default behavior for `serialize_entry` is fine.
+impl ser::SerializeMap for Serializer {
+    type Ok = ();
+    type Error = Error;
+
+    // The Serde data model allows map keys to be any serializable type. JSON
+    // only allows string keys so the implementation below will produce invalid
+    // JSON if the key serializes as something other than a string.
+    //
+    // A real JSON serializer would need to validate that map keys are strings.
+    // This can be done by using a different Serializer to serialize the key
+    // (instead of `&mut **self`) and having that other serializer only
+    // implement `serialize_str` and return an error on any other data type.
+    fn serialize_key<T>(&mut self, _key: &T) -> Result<()>
+    where
+        T: ?Sized + ser::Serialize,
+    {
+        Err(anyhow::anyhow!("Map not supported").into())
+    }
+
+    // It doesn't make a difference whether the colon is printed at the end of
+    // `serialize_key` or at the beginning of `serialize_value`. In this case
+    // the code is a bit simpler having it here.
+    fn serialize_value<T>(&mut self, _value: &T) -> Result<()>
+    where
+        T: ?Sized + ser::Serialize,
+    {
+        Err(anyhow::anyhow!("Map not supported").into())
+    }
+
+    fn end(self) -> Result<()> {
+        Ok(())
+    }
+}
+
+// Structs are like maps in which the keys are constrained to be compile-time
+// constant strings.
+impl ser::SerializeStruct for Serializer {
+    type Ok = ();
+    type Error = Error;
+
+    fn serialize_field<T>(&mut self, _key: &'static str, _value: &T) -> Result<()>
+    where
+        T: ?Sized + ser::Serialize,
+    {
+        Err(anyhow::anyhow!("Struct not supported").into())
+    }
+
+    fn end(self) -> Result<()> {
+        Ok(())
+    }
+}
+
+// Similar to `SerializeTupleVariant`, here the `end` method is responsible for
+// closing both of the curly braces opened by `serialize_struct_variant`.
+impl ser::SerializeStructVariant for Serializer {
+    type Ok = ();
+    type Error = Error;
+
+    fn serialize_field<T>(&mut self, _key: &'static str, _value: &T) -> Result<()>
+    where
+        T: ?Sized + ser::Serialize,
+    {
+        Err(anyhow::anyhow!("Struct variant not supported").into())
+    }
+
+    fn end(self) -> Result<()> {
+        Ok(())
+    }
+}
