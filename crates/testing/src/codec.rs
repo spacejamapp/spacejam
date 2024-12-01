3f3d32888af4b4e13a17c8554e26250b76607320
clearloop <tianyi.gc@gmail.com>
2024-12-01 18:20:44 +0700

    chore(testing): generate tests with macro

--- crates/testing/src/codec.rs
+++ crates/testing/src/codec.rs
@@ -17,162 +17,70 @@ use core::{
         WorkItem, WorkItemJson, WorkPackage, WorkPackageJson,
     },
 };
+use paste::paste;
 
-#[test]
-fn decode_assurances_extrinsic() -> Result<()> {
-    let json = include_str!("../jamtestvectors/codec/data/assurances_extrinsic.json");
-    let _data = include_bytes!("../jamtestvectors/codec/data/assurances_extrinsic.bin");
-
-    let _assurances: Vec<AvailAssurance> = serde_json::from_str::<Vec<AvailAssuranceJson>>(json)?
-        .into_iter()
-        .map(TryInto::try_into)
-        .collect::<Result<Vec<_>>>()?;
-
-    // TODO: implement the jamcodec
-    // assert_eq!(assurances.encode(), data);
-    Ok(())
+macro_rules! load_codec_data {
+    ($name:ident) => {{
+        let json = include_str!(concat!(
+            "../jamtestvectors/codec/data/",
+            stringify!($name),
+            ".json"
+        ));
+        let data = include_bytes!(concat!(
+            "../jamtestvectors/codec/data/",
+            stringify!($name),
+            ".bin"
+        ));
+        (json, data)
+    }};
+    ($name:ident, $json:ident, $dest:ident) => {
+        paste! {
+            #[test]
+            fn [<decode_ $name>]() -> Result<()> {
+                let (json, _data) = load_codec_data!($name);
+                let _decoded: $dest = serde_json::from_str::<$json>(json)?.try_into()?;
+                Ok(())
+            }
+        }
+    };
+    ($name:ident, $json:ty, $dest:ty) => {
+        paste! {
+            #[test]
+            fn [<decode_ $name>]() -> Result<()> {
+                let (json, _data) = load_codec_data!($name);
+                let _decoded: $dest = serde_json::from_str::<$json>(json)?
+                    .into_iter()
+                    .map(TryInto::try_into)
+                    .collect::<Result<Vec<_>>>()?;
+                Ok(())
+            }
+        }
+    };
+    ($(($name:ident, $json:ident, $dest:ident)),*) => {
+        $(load_codec_data!($name, $json, $dest);)*
+    };
+    ($(($name:ident, $json:ty, $dest:ty)),*) => {
+        $(load_codec_data!($name, $json, $dest);)*
+    };
 }
 
-#[test]
-fn decode_block() -> Result<()> {
-    let json = include_str!("../jamtestvectors/codec/data/block.json");
-    let _data = include_bytes!("../jamtestvectors/codec/data/block.bin");
-
-    let _block: Block = serde_json::from_str::<BlockJson>(json)?.try_into()?;
-
-    // TODO: implement the jamcodec
-    // assert_eq!(block.encode(), data);
-    Ok(())
+load_codec_data! {
+    (assurances_extrinsic, Vec<AvailAssuranceJson>, Vec<AvailAssurance>),
+    (guarantees_extrinsic, Vec<ReportGuaranteeJson>, GuaranteesExtrinsic),
+    (preimages_extrinsic, Vec<PreimageJson>, PreimagesExtrinsic),
+    (tickets_extrinsic, Vec<TicketEnvelopeJson>, TicketsExtrinsic)
 }
 
-#[test]
-fn decode_disputes_extrinsic() -> Result<()> {
-    let json = include_str!("../jamtestvectors/codec/data/disputes_extrinsic.json");
-    let _data = include_bytes!("../jamtestvectors/codec/data/disputes_extrinsic.bin");
-
-    let _disputes: DisputesExtrinsic =
-        serde_json::from_str::<DisputesExtrinsicJson>(json)?.try_into()?;
-    Ok(())
-}
-
-#[test]
-fn decode_extrinsic() -> Result<()> {
-    let json = include_str!("../jamtestvectors/codec/data/extrinsic.json");
-    let _data = include_bytes!("../jamtestvectors/codec/data/extrinsic.bin");
-
-    let _extrinsic: Extrinsic = serde_json::from_str::<ExtrinsicJson>(json)?.try_into()?;
-    Ok(())
-}
-
-#[test]
-fn decode_guarantees_extrinsic() -> Result<()> {
-    let json = include_str!("../jamtestvectors/codec/data/guarantees_extrinsic.json");
-    let _data = include_bytes!("../jamtestvectors/codec/data/guarantees_extrinsic.bin");
-
-    let _guarantees: GuaranteesExtrinsic = serde_json::from_str::<Vec<ReportGuaranteeJson>>(json)?
-        .into_iter()
-        .map(TryInto::try_into)
-        .collect::<Result<Vec<_>>>()?;
-
-    Ok(())
-}
-
-#[test]
-fn decode_header_0() -> Result<()> {
-    let json = include_str!("../jamtestvectors/codec/data/header_0.json");
-    let _data = include_bytes!("../jamtestvectors/codec/data/header_0.bin");
-
-    let _header: Header = serde_json::from_str::<HeaderJson>(json)?.try_into()?;
-    Ok(())
-}
-
-#[test]
-fn decode_header_1() -> Result<()> {
-    let json = include_str!("../jamtestvectors/codec/data/header_1.json");
-    let _data = include_bytes!("../jamtestvectors/codec/data/header_1.bin");
-
-    let _header: Header = serde_json::from_str::<HeaderJson>(json)?.try_into()?;
-    Ok(())
-}
-
-#[test]
-fn decode_preimages_extrinsic() -> Result<()> {
-    let json = include_str!("../jamtestvectors/codec/data/preimages_extrinsic.json");
-    let _data = include_bytes!("../jamtestvectors/codec/data/preimages_extrinsic.bin");
-
-    let _preimages: PreimagesExtrinsic = serde_json::from_str::<Vec<PreimageJson>>(json)?
-        .into_iter()
-        .map(TryInto::try_into)
-        .collect::<Result<Vec<_>>>()?;
-    Ok(())
-}
-
-#[test]
-fn decode_refine_context() -> Result<()> {
-    let json = include_str!("../jamtestvectors/codec/data/refine_context.json");
-    let _data = include_bytes!("../jamtestvectors/codec/data/refine_context.bin");
-
-    let _refine_context: RefineContext =
-        serde_json::from_str::<RefineContextJson>(json)?.try_into()?;
-
-    Ok(())
-}
-
-#[test]
-fn decode_tickets_extrinsic() -> Result<()> {
-    let json = include_str!("../jamtestvectors/codec/data/tickets_extrinsic.json");
-    let _data = include_bytes!("../jamtestvectors/codec/data/tickets_extrinsic.bin");
-
-    let _tickets: TicketsExtrinsic = serde_json::from_str::<Vec<TicketEnvelopeJson>>(json)?
-        .into_iter()
-        .map(TryInto::try_into)
-        .collect::<Result<Vec<_>>>()?;
-    Ok(())
-}
-
-#[test]
-fn decode_work_item() -> Result<()> {
-    let json = include_str!("../jamtestvectors/codec/data/work_item.json");
-    let _data = include_bytes!("../jamtestvectors/codec/data/work_item.bin");
-
-    let _work_item: WorkItem = serde_json::from_str::<WorkItemJson>(json)?.try_into()?;
-
-    Ok(())
-}
-
-#[test]
-fn decode_work_package() -> Result<()> {
-    let json = include_str!("../jamtestvectors/codec/data/work_package.json");
-    let _data = include_bytes!("../jamtestvectors/codec/data/work_package.bin");
-
-    let _work_package: WorkPackage = serde_json::from_str::<WorkPackageJson>(json)?.try_into()?;
-
-    Ok(())
-}
-
-#[test]
-fn decode_work_report() -> Result<()> {
-    let json = include_str!("../jamtestvectors/codec/data/work_report.json");
-    let _data = include_bytes!("../jamtestvectors/codec/data/work_report.bin");
-
-    let _work_report: WorkReport = serde_json::from_str::<WorkReportJson>(json)?.try_into()?;
-    Ok(())
-}
-
-#[test]
-fn decode_work_result_0() -> Result<()> {
-    let json = include_str!("../jamtestvectors/codec/data/work_result_0.json");
-    let _data = include_bytes!("../jamtestvectors/codec/data/work_result_0.bin");
-
-    let _work_result: WorkResult = serde_json::from_str::<WorkResultJson>(json)?.try_into()?;
-    Ok(())
-}
-
-#[test]
-fn decode_work_result_1() -> Result<()> {
-    let json = include_str!("../jamtestvectors/codec/data/work_result_1.json");
-    let _data = include_bytes!("../jamtestvectors/codec/data/work_result_1.bin");
-
-    let _work_result: WorkResult = serde_json::from_str::<WorkResultJson>(json)?.try_into()?;
-    Ok(())
+load_codec_data! {
+    (block, BlockJson, Block),
+    (disputes_extrinsic, DisputesExtrinsicJson, DisputesExtrinsic),
+    (extrinsic, ExtrinsicJson, Extrinsic),
+    (header_0, HeaderJson, Header),
+    (header_1, HeaderJson, Header),
+    (refine_context, RefineContextJson, RefineContext),
+    (work_item, WorkItemJson, WorkItem),
+    (work_package, WorkPackageJson, WorkPackage),
+    (work_report, WorkReportJson, WorkReport),
+    (work_result_0, WorkResultJson, WorkResult),
+    (work_result_1, WorkResultJson, WorkResult)
 }
