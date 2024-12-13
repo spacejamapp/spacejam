8c3e85a1114d5511150b07e833b83d7b4258bb54
clearloop <tianyi.gc@gmail.com>
2024-12-13 15:15:22 +0700

    feat(report): validate block info and core index

--- crates/report/src/lib.rs
+++ crates/report/src/lib.rs
@@ -3,8 +3,8 @@
 use {
     error::{Error, Result},
     score::{
-        extrinsic::ValidatorSignature, work::AvailabilityAssignment, CORES_COUNT,
-        VALIDATORS_SUPER_MAJORITY,
+        extrinsic::ReportGuarantee, misc::TimeSlot, work::AvailabilityAssignment, CORES_COUNT,
+        VALIDATORS_COUNT, VALIDATORS_SUPER_MAJORITY,
     },
     state::{Input, Output, ReportedPackage, State},
 };
@@ -23,57 +23,42 @@ impl Handler {
     pub fn handle(&mut self, input: Input) -> Result<Output> {
         let mut reported = Vec::new();
         let mut reporters = Vec::new();
-
-        // Validate guarantees are ordered by core index
-        for window in input.guarantees.windows(2) {
-            if window[0].report.core_index >= window[1].report.core_index {
-                return Err(Error::NotSortedOrUniqueGuarantors);
-            }
-        }
+        let code_hashes = self
+            .prev
+            .services
+            .iter()
+            .map(|s| s.info.code_hash)
+            .collect::<Vec<_>>();
+        let service_ids = self.prev.services.iter().map(|s| s.id).collect::<Vec<_>>();
 
         // Process each guarantee
-        for guarantee in input.guarantees {
-            let core_idx = guarantee.report.core_index as usize;
+        for (core_index, guarantee) in input.guarantees.into_iter().enumerate() {
+            self.validate_core(input.slot, core_index, &guarantee)?;
 
-            // Validate core index
-            if core_idx >= CORES_COUNT {
-                return Err(Error::BadCoreIndex);
+            if guarantee
+                .report
+                .results
+                .iter()
+                .any(|r| !code_hashes.contains(&r.code_hash))
+            {
+                return Err(Error::BadCodeHash);
             }
 
-            // Check if core already has a pending report that hasn't timed out
-            if let Some(assignment) = &self.next.avail_assignments[core_idx] {
-                if input.slot <= assignment.timeout {
-                    return Err(Error::CoreEngaged);
-                }
+            if guarantee
+                .report
+                .results
+                .iter()
+                .any(|r| !service_ids.contains(&r.service_id))
+            {
+                return Err(Error::BadServiceId);
             }
+
+            self.validate_block(&guarantee)?;
+            self.validate_signatures(&guarantee)?;
 
             // Validate report's authorizer is in the authorization pool
-            if !self.next.auth_pools[core_idx].contains(&guarantee.report.authorizer_hash) {
+            if !self.next.auth_pools[core_index].contains(&guarantee.report.authorizer_hash) {
                 return Err(Error::CoreUnauthorized);
-            }
-
-            // Require at least 2/3 guarantors
-            if guarantee.signatures.len() < VALIDATORS_SUPER_MAJORITY as usize {
-                return Err(Error::InsufficientGuarantees);
-            }
-
-            // Validate guarantors' signatures
-            for ValidatorSignature {
-                validator_index,
-                signature: _,
-            } in &guarantee.signatures
-            {
-                let validator_index = *validator_index as usize;
-                if validator_index >= self.next.curr_validators.len() {
-                    return Err(Error::BadValidatorIndex);
-                }
-
-                // TODO: implement signature verification
-                /* if !self.next.curr_validators[validator_index]
-                    .verify_guarantee(&guarantee.report, &signature)
-                {
-                    return Err(Error::BadSignature);
-                } */
             }
 
             // Record reported package
@@ -94,7 +79,7 @@ impl Handler {
             };
 
             // Update state
-            self.next.avail_assignments[core_idx] = Some(assignment);
+            self.next.avail_assignments[core_index] = Some(assignment);
 
             // Record reporters (guarantors)
             reporters.extend(guarantee.signatures.iter().map(|sig| {
@@ -108,6 +93,78 @@ impl Handler {
             reported,
             reporters,
         })
+    }
+
+    fn validate_block(&self, guarantee: &ReportGuarantee) -> Result<()> {
+        let Some(block) = self
+            .prev
+            .recent_blocks
+            .iter()
+            .find(|b| b.header_hash == guarantee.report.context.anchor)
+        else {
+            return Err(Error::AnchorNotRecent);
+        };
+
+        // Validate state root
+        if block.state_root != guarantee.report.context.state_root {
+            return Err(Error::BadStateRoot);
+        }
+
+        // Validate beefy mmr root
+        //
+        // FIXME: This verification could be wrong.
+        /* if block
+            .mmr
+            .peaks
+            .get(guarantee.report.context.lookup_anchor_slot as usize)
+            .cloned()
+            .flatten()
+            .ok_or(Error::BadBeefyMmrRoot)?
+            != guarantee.report.context.beefy_root
+        {
+            return Err(Error::BadBeefyMmrRoot);
+        } */
+        Ok(())
+    }
+
+    /// Validate core assignments
+    fn validate_core(
+        &self,
+        slot: TimeSlot,
+        core_index: usize,
+        guarantee: &ReportGuarantee,
+    ) -> Result<()> {
+        if guarantee.report.core_index >= CORES_COUNT as u16 {
+            return Err(Error::BadCoreIndex);
+        }
+
+        if guarantee.report.core_index != core_index as u16 {
+            return Err(Error::NotSortedOrUniqueGuarantors);
+        }
+
+        // Check if core already has a pending report that hasn't timed out
+        if let Some(assignment) = &self.next.avail_assignments[core_index] {
+            if slot <= assignment.timeout + 1 {
+                return Err(Error::CoreEngaged);
+            }
+        }
+        Ok(())
+    }
+
+    fn validate_signatures(&self, guarantee: &ReportGuarantee) -> Result<()> {
+        for (_, sig) in guarantee.signatures.iter().enumerate() {
+            let validator_index = sig.validator_index as usize;
+            if validator_index >= VALIDATORS_COUNT as usize {
+                return Err(Error::BadValidatorIndex);
+            }
+        }
+
+        // Require at least 2/3 guarantors
+        if guarantee.signatures.len() < VALIDATORS_SUPER_MAJORITY as usize {
+            return Err(Error::InsufficientGuarantees);
+        }
+
+        Ok(())
     }
 }
 
