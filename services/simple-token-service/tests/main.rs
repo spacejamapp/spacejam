083fc01cb5194346e010b61e86ed1a2709a79f86
clearloop <tianyi.gc@gmail.com>
2025-07-28 22:11:09 +0800

    feat(pmvt): introduce refine interfaces

--- services/simple-token-service/tests/main.rs
+++ services/simple-token-service/tests/main.rs
@@ -1,5 +1,6 @@
 //! Basic VM tests
 
+use podec::Encode;
 use simple_token_service::{Instruction, SERVICE};
 use testing::Jam;
 
@@ -8,14 +9,16 @@ const SERVICE_ID: u32 = 500;
 #[test]
 fn test_refine() {
     testing::util::init_logger();
-
     let mut jam = Jam::default();
     jam.add_service(SERVICE_ID, SERVICE.to_vec());
 
+    // 1. send a mint instruction
+    let instr = vec![Instruction::Mint { to: 0, amount: 100 }];
     let package = jam
-        .send(SERVICE_ID, vec![])
+        .send(SERVICE_ID, instr.encode())
         .expect("failed to send work item");
 
+    // 2. refine the package
     let refined = jam.refine(&package).expect("failed to refine");
     assert!(
         refined.executed.is_ok(),
