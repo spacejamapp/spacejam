0fb03204c2da7872c8eed541c990ad21885b33cf
clearloop <tianyi.gc@gmail.com>
2025-08-29 15:50:38 +0800

    fix(pvmc): setjmp on osx again

--- crates/pvm/compiler/csrc/setjmp.rs.c
+++ crates/pvm/compiler/csrc/setjmp.rs.c
@@ -1,14 +1,16 @@
 #include "setjmp.rs.h"
 #include <stdlib.h>
 
-bool setjmp_rs(void **buf_storage, bool (*body)(void *, void *), void *payload, void *callee __attribute__((unused))) {
-    // Allocate jmp_buf on heap to ensure it remains valid
+bool setjmp_rs(void **buf_storage, bool (*body)(void *, void *), void *payload, void *callee __attribute__((unused)))
+{
     platform_jmp_buf *buf = (platform_jmp_buf *)malloc(sizeof(platform_jmp_buf));
-    if (buf == NULL) {
+    if (buf == NULL)
+    {
         return false;
     }
-    
-    if (platform_setjmp(*buf) != 0) {
+
+    if (platform_setjmp(*buf) != 0)
+    {
         free(buf);
         return false;
     }
@@ -18,18 +20,21 @@ bool setjmp_rs(void **buf_storage, bool (*body)(void *, void *), void *payload, 
     return result;
 }
 
-void longjmp_rs(void *jmp_buf_ptr) {
+void longjmp_rs(void *jmp_buf_ptr)
+{
     platform_jmp_buf *buf = (platform_jmp_buf *)jmp_buf_ptr;
     platform_longjmp(*buf, 1);
 }
 
 #ifdef __APPLE__
-int install_signal_handlers(void (*handler)(int, pvm_siginfo_t *, void *)) {
+int install_signal_handlers(void (*handler)(int, pvm_siginfo_t *, void *))
+{
     struct sigaction sa;
 
     // Zero out the structure
     char *p = (char *)&sa;
-    for (unsigned int i = 0; i < sizeof(struct sigaction); i++) {
+    for (unsigned int i = 0; i < sizeof(struct sigaction); i++)
+    {
         p[i] = 0;
     }
 
@@ -38,17 +43,20 @@ int install_signal_handlers(void (*handler)(int, pvm_siginfo_t *, void *)) {
     sigemptyset(&sa.sa_mask);
 
     // Install SIGSEGV handler (primary signal for memory violations)
-    if (sigaction(PVM_SIGSEGV, &sa, NULL) != 0) {
+    if (sigaction(PVM_SIGSEGV, &sa, NULL) != 0)
+    {
         return -1;
     }
 
     // Install SIGBUS handler (macOS often sends SIGBUS instead of SIGSEGV)
-    if (sigaction(PVM_SIGBUS, &sa, NULL) != 0) {
+    if (sigaction(PVM_SIGBUS, &sa, NULL) != 0)
+    {
         return -2;
     }
 
     // Install SIGTRAP handler (macOS might send SIGTRAP for null pointer access)
-    if (sigaction(PVM_SIGTRAP, &sa, NULL) != 0) {
+    if (sigaction(PVM_SIGTRAP, &sa, NULL) != 0)
+    {
         return -3;
     }
 
@@ -56,17 +64,19 @@ int install_signal_handlers(void (*handler)(int, pvm_siginfo_t *, void *)) {
 }
 #else
 // Linux signal handler (currently just SIGSEGV, expandable for future signals)
-int install_signal_handlers(void (*handler)(int, pvm_siginfo_t*, void*)) {
+int install_signal_handlers(void (*handler)(int, pvm_siginfo_t *, void *))
+{
     struct sigaction sa = {0};
-    sa.sa_sigaction = (void (*)(int, siginfo_t*, void*))handler;
+    sa.sa_sigaction = (void (*)(int, siginfo_t *, void *))handler;
     sa.sa_flags = PVM_SA_SIGINFO | PVM_SA_NODEFER;
     sigemptyset(&sa.sa_mask);
-    
+
     // Install SIGSEGV handler
-    if (sigaction(PVM_SIGSEGV, &sa, NULL) != 0) {
+    if (sigaction(PVM_SIGSEGV, &sa, NULL) != 0)
+    {
         return -1;
     }
-    
+
     return 0;
 }
 #endif
