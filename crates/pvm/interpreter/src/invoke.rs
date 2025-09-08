877ac8fc3d46fb6777ec3199fa38f78fbaba982f
clearloop <tianyi.gc@gmail.com>
2025-09-08 08:10:21 +0800

    feat(pvm): no more block based execution

--- crates/pvm/interpreter/src/invoke.rs
+++ crates/pvm/interpreter/src/invoke.rs
@@ -24,45 +24,34 @@ impl Interpreter {
         };
         let blob = program.blob()?;
         interp.table = blob.jump_table.to_vec();
-        // crate::analyse::analyse(program)?;
 
         // interpret the program
         let mut reader = blob.reader().with_position(pc);
         loop {
-            let block = reader.read_block()?;
-            if block.is_empty() {
+            let Ok(instr) = reader.read() else {
                 break;
-            }
+            };
 
-            tracing::trace!(
-                "pos={:<6} gas={:<6} regs={:?}",
-                interp.pc,
-                interp.context.gas,
-                interp.context.registers
-            );
-            for instr in block {
-                interp.pc = instr.range.start;
-                match interp.step(&instr) {
-                    Reason::Continue => {
-                        if let Some(target) = interp.jump.take() {
-                            interp.pc = target;
-                            reader.set_position(target);
-                            break;
-                        }
+            interp.pc = instr.range.start;
+            match interp.step(&instr) {
+                Reason::Continue => {
+                    if let Some(target) = interp.jump.take() {
+                        interp.pc = target;
+                        reader.set_position(target);
+                    }
 
-                        continue;
-                    }
-                    Reason::HostCall(call) => {
-                        let mut context = interp.context.ctx(&mut ctx);
-                        let reason = host::call(call, &mut context);
-                        interp.context.registers = context.registers;
-                        if reason != Reason::Continue {
-                            return Ok(interp.result(ctx, initial_gas, reason));
-                        }
-                    }
-                    reason => {
+                    continue;
+                }
+                Reason::HostCall(call) => {
+                    let mut context = interp.context.ctx(&mut ctx);
+                    let reason = host::call(call, &mut context);
+                    interp.context.registers = context.registers;
+                    if reason != Reason::Continue {
                         return Ok(interp.result(ctx, initial_gas, reason));
                     }
+                }
+                reason => {
+                    return Ok(interp.result(ctx, initial_gas, reason));
                 }
             }
         }
