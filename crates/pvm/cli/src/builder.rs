054a8a66981276e3b1cbae3a747fcf41077f9b4f
clearloop <tianyi.gc@gmail.com>
2025-09-19 15:16:34 +0800

    chore(pvm): upgrade the toolchain for jade

--- crates/pvm/cli/src/builder.rs
+++ crates/pvm/cli/src/builder.rs
@@ -4,7 +4,7 @@
 #![allow(clippy::unwrap_used)]
 
 // If you update this, you should also update the toolchain installed by .github/workflows/rust.yml
-const TOOLCHAIN: &str = "1.86.0";
+const TOOLCHAIN: &str = "1.90.0";
 
 use crate::manifest;
 use codec::Encode;
