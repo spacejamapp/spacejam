c1838e57b310af59f47a8eb68681505db23e4cc4
clearloop <tianyi.gc@gmail.com>
2025-09-23 23:49:36 +0800

    chore(workspace): publish shared libraries

--- crates/vm/sys/build.rs
+++ crates/vm/sys/build.rs
@@ -1,13 +1,22 @@
 //! Link spacevm to this sys library
 
-// use std::process::Command;
+use std::{env, fs, path::PathBuf, process::Command};
 
-use std::{env, path::PathBuf};
+fn main() -> std::io::Result<()> {
+    let out = PathBuf::from(env::var("OUT_DIR").unwrap());
+    Command::new("curl")
+        .args(&[
+            "-L",
+            "https://github.com/spacejamapp/specjam/releases/download/0.7.0-pre.9/libspacevm.a",
+            "-o",
+            "libspacevm.a",
+        ])
+        .status()?;
 
-fn main() {
-    let _out = env::var("OUT_DIR").unwrap();
-    let lib: PathBuf = env::current_dir().unwrap().join("../../../target/release");
-    println!("cargo:rustc-link-search=native={}", lib.display());
+    let lib = out.join("libspacevm.a");
+    fs::rename("libspacevm.a", &lib)?;
+    println!("cargo:rustc-link-search=native={}", out.display());
     println!("cargo:rustc-link-lib=static=spacevm");
     println!("cargo:rerun-if-changed=build.rs");
+    Ok(())
 }
