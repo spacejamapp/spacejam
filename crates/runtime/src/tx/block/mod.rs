0404e27e024fd23284a1c41b3d3a1231af1ed785
clearloop <tianyi.gc@gmail.com>
2026-01-15 21:54:50 +0800

    feat(runtime): introduce TestChain for saving memory

--- crates/runtime/src/tx/block/mod.rs
+++ crates/runtime/src/tx/block/mod.rs
@@ -1,10 +1,14 @@
 //! Block validations
 
-use crate::{Storage, storage::Column, tx};
+use crate::{
+    Storage,
+    storage::{Column, MemoryDb},
+    tx,
+};
 use anyhow::Result;
 use pvm::Pvm;
-use score::Block;
-use std::sync::Arc;
+use score::{Block, OpaqueHash};
+use std::{collections::HashMap, sync::Arc};
 
 pub mod header;
 pub mod history;
@@ -26,5 +30,61 @@ pub fn process<Vm: Pvm>(block: Block, storage: Arc<impl Storage>) -> Result<()> 
             storage.commit(Column::State, diff)?;
             Ok(())
         }
+    }
+}
+
+/// DEVELOPMENT: A test chain for processing fuzz blocks.
+pub struct TestChain {
+    /// The finalized head of the chain.
+    pub finalized: OpaqueHash,
+
+    /// The data of the chain.
+    pub data: Arc<MemoryDb>,
+
+    /// The forks and their states.
+    pub forks: HashMap<OpaqueHash, HashMap<Vec<u8>, Vec<u8>>>,
+}
+
+impl TestChain {
+    /// Initialize the chain with the given block.
+    pub fn new<Vm: Pvm>(block: Block) -> anyhow::Result<Self> {
+        let head = block.header.hash();
+        let data = Arc::new(MemoryDb::default());
+        let mut forks = HashMap::new();
+
+        // import the genesis block
+        self::process::<Vm>(block, data.clone())?;
+        forks.insert(head, data.deep_clone());
+        Ok(Self {
+            finalized: head,
+            data,
+            forks,
+        })
+    }
+
+    /// Import a new block to the chain.
+    pub fn import<Vm: Pvm>(&mut self, block: Block) -> anyhow::Result<()> {
+        let head = block.header.hash();
+        let parent = block.header.parent;
+        let guard = Arc::new(self.data.dup());
+        let mut pstate = None;
+
+        // find the parent of the block
+        if let Some(state) = self.forks.get(&parent) {
+            pstate = Some(state.clone());
+            guard.reset(state.clone());
+        }
+
+        // process the block
+        self::process::<Vm>(block, guard.clone())?;
+        if let Some(pstate) = pstate {
+            self.finalized = parent;
+            self.data.reset(pstate);
+            self.forks.clear();
+        }
+
+        // update the forks
+        self.forks.insert(head, guard.deep_clone());
+        Ok(())
     }
 }
